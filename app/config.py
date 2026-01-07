5026beb2425d5f8c09c292831ac43d51e448ea85
vulicasnkrs-lang <vulicasnkrs@gmail.com>
2026-01-07 13:59:29 +0300

    Delete app/config.py

--- app/config.py
+++
@@ -1,5 +0,0 @@
-# app/config.py
-
-BOT_TOKEN = "8300602711:AAFRLntEhgV6Rep6six2vzge6_qY7DpK8og"
-ADMIN_ID = 1426577785  # Ваш Telegram ID
-BASE_URL = "https://sneaker-shop-r7fa.onrender.com"  # URL вашего Render Web Service
